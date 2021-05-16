b73d939134788f7413c48ab0914ee9d083f6fcef
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-05-16 17:11:09 +0300

    latest

--- export.bat
+++ export.bat
@@ -1,2 +1,6 @@
-cd %cd%/tools/bin/
-loader.exe -v -t64 -snsih_200.bin -o %cd%/../../build/nanopim3/kernel8_nsih.img %cd%/../../build/nanopim3/kernel8.img
+ssh rock64@rock64.local rm -rf /home/rock64/temp; mkdir /home/rock64/temp
+scp -r %cd%/ rock64@rock64.local:/home/rock64/temp
+@rem scp -r %cd% rock64@rock64.local:/home/rock64/projects
+
+@REM cd %cd%/tools/bin/
+@REM loader.exe -v -t64 -snsih_200.bin -o %cd%/../../build/nanopim3/kernel8_nsih.img %cd%/../../build/nanopim3/kernel8.img
